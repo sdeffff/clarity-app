740e5caf49d7c5457095818cf737ef4e0757c610
Maksym <maxpavliy@gmail.com>
2025-03-12 23:20:53 +0100

    header fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,6 +5,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormField } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,5 +15,13 @@ import { MatButton } from '@angular/material/button';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private router: Router) { };
 
+  protected headerStyles = "background: transparent";
+
+  ngOnInit(): void {
+    if(!(this.router.url === "/")) {
+      this.headerStyles = "background: #191D21";
+    }
+  }  
 }
