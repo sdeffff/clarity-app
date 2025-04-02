386a182e57d4ce711599a6f93577750abfca9c16
Maksym <maxpavliy@gmail.com>
2025-04-02 13:56:38 +0200

    removed some unnecessary preloaders from the app

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,28 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AfterViewInit } from '@angular/core';
-
-import { NgIf } from '@angular/common';
-
 //Angular material:
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from '@angular/material/input';
 
-import { PreloaderComponent } from '../technical/preloader/preloader.component';
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatButtonModule, MatInputModule, PreloaderComponent, NgIf],
+  imports: [MatButtonModule, MatInputModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements AfterViewInit {
-  protected isLoading: boolean = true;
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 800);
-  }
+export class HomeComponent {
 }
